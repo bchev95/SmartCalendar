ad2a3b7e2e04e2c1e001f9a9f629edaf6f0c26c9
Lauren Wonicker-Cook <lwonickercook@ufl.edu>
2018-03-23 12:41:26 -0400

    Added more classes to the header file

--- SmartCalendar.h
+++ SmartCalendar.h
@@ -10,18 +10,73 @@
 using namespace std;
 
 class Event {
-private:
+protected:
     string title;
     string description;
-    int time[2];
+    string location;
+    int startTime[2];
+    int endTime[2];
+    int date[3];
+    int eventType;
 public:
     Event();
     string getTitle();
     void setTitle(string t);
     string getDescription();
     void setDescription(string d);
-    int getHour();
-    int getMinute();
-    void setTime(int hour, int minute);
+    string getLocation();
+    void setLocation(string l);
+    int getStartHour();
+    int getStartMinute();
+    void setStartTime(int hour, int minute);
+    int getEndHour();
+    int getEndMinute();
+    void setEndTime(int hour, int minute);
+    int getMonth();
+    int getDay();
+    int getYear();
+    void setDate(int d[3]);
+    int getEventType();
+    void setEventType(int e);
     void printEvent();
+};
+
+class RepeatingEvent: public Event
+{
+private:
+    int dayOfWeek;
+    int startDate[3];   //The first occurrence of the event
+    int endDate[3];     //The last occurrence of the event
+public:
+    RepeatingEvent();
+    int getDayOfWeek();
+    void setDayOfWeek(int dw);
+    int getStartMonth();
+    int getStartDay();
+    int getStartYear();
+    void setStartDate(int d[3]);
+    int getEndMonth();
+    int getEndDay();
+    int getEndYear();
+    void setEndDate(int d[3]);
+};
+
+class Day
+{
+
+};
+
+class Week
+{
+
+};
+
+class Month
+{
+
+};
+
+class Year
+{
+
 };
